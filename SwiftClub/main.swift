32712560afbc3b22a0b29aabe80d90fd909f6eae
Alexey Spektoruk <AlexeySpektoruk@gmail.com>
2018-07-11 00:00:53 +0300

    Initial Commit

---
+++ SwiftClub/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftClub
+//
+//  Created by Alexey Spektoruk on 7/11/18.
+//  Copyright © 2018 Alexey Spektoruk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
