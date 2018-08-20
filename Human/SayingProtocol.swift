9b3fe927615059e28801f7067ff5bec57f221702
Alexey Spektoruk <alexeyspektoruk@gmail.com>
2018-08-21 01:18:59 +0300

    human002

---
+++ Human/SayingProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  SayingProtocol.swift
+//  Human
+//
+//  Created by Alexey Spektoruk on 8/21/18.
+//  Copyright © 2018 Alexey Spektoruk. All rights reserved.
+//
+
+import Foundation
+
+protocol Saying {
+    func sayName()
+    func sayGender()
+    func sayAge()
+}
